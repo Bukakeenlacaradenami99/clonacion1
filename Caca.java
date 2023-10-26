cc7042b9bedca20e6126ce2553fbe3cb34abf76b
Bukakeenlacaradenami99 <cristianrmn93@gmail.com>
2023-10-26 18:07:20 +0200

    añadiendo programas

---
+++ Caca.java
@@ -0,0 +1,26 @@
+public class Caca {
+
+  private  int puta;
+    public Caca(){
+
+
+
+    }
+
+    public void dimeperra(int p){
+        puta=p;
+        System.out.println("Mi total de putas es de: " + puta);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Uso_perra.java
@@ -0,0 +1,33 @@
+public class Uso_perra {
+
+
+    public static void main(Sring[]args){
+
+        Caca c=new Caca();
+
+        c.dimeperra(3);
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

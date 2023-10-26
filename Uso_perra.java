74a08d863b52a193301a82c8b0f6023cd1eda984
Bukakeenlacaradenami99 <cristianrmn93@gmail.com>
2023-10-26 18:13:16 +0200

    poniendo color al programa

--- Uso_perra.java
+++ Uso_perra.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
 public class Uso_perra {
 
 
     public static void main(String[] args){
 
         Caca c=new Caca();
-
-        c.dimeperra(3);
+        System.out.println("Introduce el número de putas del sketch: ");
+        Scanner sc=new Scanner(System.in);
+        c.dimeperra(sc.nextInt());
 
 
 
